e6c5f060a1d9dfbd66a433b469831222ae8b746a
Jingxian12 <152453399+Jingxian12@users.noreply.github.com>
2026-05-18 04:07:48 +0800

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+import streamlit as st
+import joblib
+import pandas as pd
 
+# Load model
+model = joblib.load("model.pkl")
+
+# Title
+st.title("Student Depression Prediction")
+
+# User inputs
+age = st.number_input("Age")
+academic_pressure = st.slider("Academic Pressure", 0, 5)
+
+# Prediction button
+if st.button("Predict"):
+
+    input_data = pd.DataFrame({
+        "age": [age],
+        "academic_pressure": [academic_pressure]
+    })
+
+    prediction = model.predict(input_data)
+
+    st.write(prediction)
