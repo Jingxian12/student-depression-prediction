44f64a66a66bc90efd5acf752eba77006422eeb3
Jingxian12 <152453399+Jingxian12@users.noreply.github.com>
2026-05-18 04:14:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,3 @@ import pandas as pd
 # Title
 st.title("Student Depression Prediction")
 
-# User inputs
-age = st.number_input("Age")
-academic_pressure = st.slider("Academic Pressure", 0, 5)
-
-# Prediction button
-if st.button("Predict"):
-
-    input_data = pd.DataFrame({
-        "age": [age],
-        "academic_pressure": [academic_pressure]
-    })
