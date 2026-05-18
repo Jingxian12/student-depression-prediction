213ef947c117cbcdb2fda4da016ffc7a4dd60b57
Jingxian12 <152453399+Jingxian12@users.noreply.github.com>
2026-05-18 22:00:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ cgpa = st.number_input("CGPA", 0.0, 10.0, 5.0)
 study_satisfaction = st.slider("Study Satisfaction", 0, 10, 5)
 sleep_duration = st.selectbox(
     "Sleep Duration",
-    ["<5 hours", "5-6 hours", "6-7 hours", "7-8 hours", ">8 hours"]
+    ["Less than 5 hours","5-6 hours","7-8 hours","More than 8 hours"]
 )
 dietary_habits = st.selectbox(
     "Dietary Habits",
-    ["Healthy", "Moderate", "Unhealthy"]
+    ["Unhealthy","Moderate","Healthy"]
 )
 
 degree = st.text_input("Degree")
